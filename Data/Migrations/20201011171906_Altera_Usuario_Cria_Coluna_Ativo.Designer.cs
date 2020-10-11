41e4f1137c25dcee8ebcfe0c79e4b7821df06af9
Gladsileny <gladsileny.santos@ivoryit.com.br>
2020-10-11 17:34:23 -0300

    Add migration Alterar Usuario criar coluna Ativo

--- Data/Migrations/20201011171906_Altera_Usuario_Cria_Coluna_Ativo.Designer.cs
+++ Data/Migrations/20201011171906_Altera_Usuario_Cria_Coluna_Ativo.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SIGASE.Models;
 
 namespace NewSIGASE.Migrations
 {
     [DbContext(typeof(SIGASEContext))]
-    partial class SIGASEContextModelSnapshot : ModelSnapshot
+    [Migration("20201011171906_Altera_Usuario_Cria_Coluna_Ativo")]
+    partial class Altera_Usuario_Cria_Coluna_Ativo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
